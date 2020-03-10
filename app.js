f08d98cadbbfad44fc1e541e0ec0914177c53f5c
Vulpie <wojtas_f@outlook.com>
2020-03-10 15:59:46 +0100

    fix: heroku fix

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 
 const app = express()
 
-const PagesRouter = require('./routes/pages')
+const PagesRouter = require('./src/routes/pages')
 
 const publicServerDirectoryPath = path.join(__dirname, '../public')
 const publicClientDirectoryPath = path.join(__dirname, '../client/public')
@@ -19,17 +19,19 @@ app.set('view engine', 'hbs')
 app.set('views', viewPath)
 hbs.registerPartials(partialPath)
 
-// app.use('/tutorial',express.static(publicServerDirectoryPath))
 app.use(express.static(publicServerDirectoryPath))
 app.use(express.static(publicClientDirectoryPath))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
-})
-
 app.use(PagesRouter)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 module.exports = app
