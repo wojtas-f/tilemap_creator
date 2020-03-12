e9c604451c5f1fba19fd8beda904a166f5017a1f
Vulpie <wojtas_f@outlook.com>
2020-03-12 20:01:36 +0100

    feat: add error message for MapSize modal

--- client/src/components/editor/modals/MapSize.js
+++ client/src/components/editor/modals/MapSize.js
@@ -5,7 +5,30 @@ import React, { Component } from 'react'
  */
 class MapSize extends Component {
     state = {
-        size: [0, 0]
+        size: [0, 0],
+        errorMessage: null
+    }
+
+    validateInput = (width, height) => {
+        let errorMessage = ''
+        if (width < 10) {
+            errorMessage += 'Width must be longer than 10. '
+        } else if (width > 30) {
+            errorMessage += 'Width can not be longer than 30. '
+        }
+
+        if (height < 10) {
+            errorMessage += 'Height must be longer than 10. '
+        } else if (height > 30) {
+            errorMessage += 'Height can not be longer than 30. '
+        }
+
+        if (errorMessage) {
+            this.setState({ errorMessage })
+            return false
+        } else {
+            return true
+        }
     }
 
     handleChange = event => {
@@ -13,14 +36,17 @@ class MapSize extends Component {
         let size = []
         size[0] = parseInt(event.target.width.value)
         size[1] = parseInt(event.target.height.value)
-        this.props.handleMapSize(size)
-        this.setState({ tileSize: size })
+        if (this.validateInput(size[0], size[1])) {
+            this.props.handleMapSize(size)
+            this.setState({ tileSize: size, errorMessage: null })
+        }
     }
 
     render() {
         return (
             <div className="modal-window">
                 <p className="modal-window_title">Map Size:</p>
+                <p className="modal-window_error">{this.state.errorMessage}</p>
                 <form
                     className="modal-window_form"
                     onSubmit={this.handleChange}
