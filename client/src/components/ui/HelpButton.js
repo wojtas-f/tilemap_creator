306874e8655b7ead25cb9e69c51c0ba780a5765e
Vulpie <wojtas_f@outlook.com>
2020-04-09 11:13:48 +0200

    feat: add button bar
    add help and github buttons

---
+++ client/src/components/ui/HelpButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class HelpButton extends Component {
+    render() {
+        return (
+            <a href="/tutorial">
+                <div className="ui-button-bar_item">?</div>
+            </a>
+        )
+    }
+}
+
+export default HelpButton
